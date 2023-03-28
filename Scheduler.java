8fa27ca43f4f9bf132e16948a67c3bc4446f86e6
Joshua Santos <joshua.santos1@students.mq.edu.au>
2023-03-29 01:56:01 +1100

    implemented getLargestServer

--- Scheduler.java
+++ Scheduler.java
@@ -1,15 +1,34 @@
+import java.io.*;
+
 public abstract class Scheduler {
     Buffer buffer;
     Message msg;
     String largestServerName;
-    Parser parse;
+    Parser parser;
+    BufferedReader input;
 
-    Scheduler(Buffer buffer, Message msg, String largestServerName, Parser parse) {
+    Scheduler(Buffer buffer, Message msg, String largestServerName, Parser parser, BufferedReader input) {
         this.buffer = buffer;
         this.msg = msg;
         this.largestServerName = largestServerName;
-        this.parse = parse;
+        this.parser = parser;
+        this.input = input;
     }
 
     public abstract void execute();
+
+    public void getLargestServer() {
+        try {
+            msg.send("GETS All");
+            if(buffer.get().contains("DATA")) {
+                msg.send("OK");
+                while(input.ready()) {
+                    buffer.update(input.readLine());
+                }
+                parser.findLargestServer(buffer.get());
+                msg.send("OK");
+            }
+            msg.send("REDY");
+        } catch (Exception e) {System.out.println("Error @ GETS All request: " + e);}
+    }
 }
