ecc17d943a2c83d399358712523bb8d6abade298
Joshua Santos <joshua.santos1@students.mq.edu.au>
2023-03-29 01:48:29 +1100

    Parser class

---
+++ Parser.java
@@ -0,0 +1,36 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Parser {
+
+    private String parse(String reg, String str, int index) {
+        Pattern p = Pattern.compile(reg);
+        Matcher m = p.matcher(str);
+        if(m.find()) {
+            return m.group(index);
+        }
+        return "";
+    }
+
+    public String findLargestServer(String msg) throws Exception {
+        try {
+            String reg = "([^ ]+)([ ])([0-9]+)";
+            return parse(reg, msg, 1);
+        } catch (Exception e) {throw new Exception("Could not find largest server ID. Error: " + e);}
+    }
+
+    public String getMaxNumServers(String msg) throws Exception {
+    try {
+        String reg = "([^ ]+)([ ])([0-9]+)";
+        return parse(reg, msg, 3);
+    } catch (Exception e) {throw new Exception("Could not find largest number of servers. Error: " + e);}
+}
+
+    public String getJobId(String msg) throws Exception {
+        try {
+            String reg = "([a-zA-Z ])+[0-9]+([ ]+[0-9]+)";
+            return parse(reg, msg, 2);
+        }
+        catch (Exception e) {throw new Exception("Could not get Job Id. Error: " + e);}
+    }
+}
