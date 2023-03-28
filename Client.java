feed67e42ebe96b04ddb4618fc5659b50a50d0e7
Joshua Santos <joshua.santos1@students.mq.edu.au>
2023-03-28 23:12:40 +1100

    removed this keyword; implemented LRR

--- Client.java
+++ Client.java
@@ -11,20 +11,21 @@ public class Client {
     private BufferedReader input;
     private DataOutputStream output;
     private String buffer;
-    private String largestServer;
+    private String largestServerName;
+    private int largestServerMax;
     
     public Client(String ip, int port) {
         this.ip = ip;
         this.port = port;
         this.buffer = "";
-        this.initalise();
+        initalise();
     }
 
     private void initalise() {
         try {
-            this.client = new Socket(this.ip, this.port);
-            this.input = new BufferedReader(new InputStreamReader(client.getInputStream()));
-            this.output = new DataOutputStream(client.getOutputStream());
+            client = new Socket(ip, port);
+            input = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            output = new DataOutputStream(client.getOutputStream());
         } catch (Exception e) {System.out.println("Error @ initalisaiton: " + e);}       
     }
 
@@ -32,43 +33,52 @@ public class Client {
         try {
             String[] msgs = {"HELO", "AUTH user", "REDY"};
             for(int i = 0; i < msgs.length; i++) {
-                this.sendMsgs(msgs[i]);
-                this.printMsgs();
+                sendMsgs(msgs[i]);
+                printMsgs();
             }
         } catch (Exception e) {System.out.println("Error @ authentication: " + e);}
     }
 
     public void getServerInfo() {
         try {
-            this.sendMsgs("GETS All");
-            this.printMsgs();
-            if(this.buffer.contains("DATA")) {
-                this.sendMsgs("OK");
-                this.buffer = this.input.readLine();
-                while(this.input.ready()) {
-                    this.buffer = this.input.readLine();
+            sendMsgs("GETS All");
+            printMsgs();
+            if(buffer.contains("DATA")) {
+                sendMsgs("OK");
+                buffer = input.readLine();
+                while(input.ready()) {
+                    buffer = input.readLine();
                 }
-                this.getFirstLargestServer(buffer);
-                System.out.println(this.largestServer);
-                this.sendMsgs("OK");
+                getLargestServer(buffer);
+                System.out.println(largestServerName);
+                sendMsgs("OK");
             }
-            this.printMsgs();
+            printMsgs();
+            sendMsgs("REDY");
+            printMsgs();
         } catch (Exception e) {System.out.println("Error @ GETS All request: " + e);}
     }
 
     public void scheduleJobs() {
         try {
-            while(!this.buffer.contains("NONE")) {
-                if(this.getJobId(buffer).contains("n/a")) {
-                    this.sendMsgs("REDY");
-                    this.printMsgs();
+            int i = 0;
+            while(!buffer.contains("NONE")) {
+                if(buffer.contains("JCPL")) {
+                    sendMsgs("REDY");
+                    printMsgs();
                 }
                 else {
-                    String jobToSchedule = "SCHD " + this.getJobId(buffer) + " " + this.largestServer + " 0";
-                    this.sendMsgs(jobToSchedule);
-                    this.printMsgs();
-                    this.sendMsgs("REDY");
-                    this.printMsgs();
+                    String jobToSchedule = "SCHD" + getJobId(buffer) + " " + largestServerName + " " + i;
+                    sendMsgs(jobToSchedule);
+                    printMsgs();
+                    Thread.sleep(2000);
+                    sendMsgs("REDY");
+                    printMsgs();
+                    Thread.sleep(2000);
+                    i++;
+                }
+                if(i > largestServerMax) {
+                    i = 0;
                 }
             }
 
@@ -77,46 +87,47 @@ public class Client {
 
     private void sendMsgs(String msg) {
         try {
-            this.output.write((msg + "\n").getBytes());
-            this.output.flush();
-            //System.out.println("Client says: " + msg);	
+            output.write((msg + "\n").getBytes());
+            output.flush();
+            System.out.println("Client says: " + msg);	
         } catch (Exception e) {System.out.println("Error @ sending message to server: " + e);}
     }
 
     private void printMsgs() {
         try {
-            buffer = this.input.readLine();
-            //System.out.println("Server says: " + buffer);
+            buffer = input.readLine();
+            System.out.println("Server says: " + buffer);
         } catch (Exception e) {System.out.println("Error @ printing server messages: " + e);}
-    }
+    }       
 
-    private void getFirstLargestServer(String msg) throws Exception {
-        Pattern reg = Pattern.compile("([^ ])+");
+    private void getLargestServer(String msg) throws Exception {
+        Pattern reg = Pattern.compile("([^ ]+)([ ])([0-9]+)");
         Matcher m = reg.matcher(msg);
         if(m.find()) {
-            this.largestServer = m.group(0);
+            largestServerName = m.group(1);
+            largestServerMax = Integer.parseInt(m.group(3));
         }
         else {
             throw new Exception("Could not find largest server ID");
         }
     }
 
-    private String getJobId(String msg) {
+    private String getJobId(String msg) throws Exception {
         Pattern reg = Pattern.compile("([a-zA-Z ])+[0-9]+([ ]+[0-9]+)");
         Matcher m = reg.matcher(msg);
         if(m.find()) {
-            if(m.group(0).contains("JCPL")){
-                return "n/a";
+            if(m.group(0).contains("JOBN")){
+                return m.group(2);
             }
-            return m.group(2);
+            return "0";
         }
-        return "0";
+        throw new Exception("Could not get Job Id");
     }
 
     public void close() {
         try {
-            this.sendMsgs("QUIT");
-            this.client.close();
+            sendMsgs("QUIT");
+            client.close();
         } catch (Exception e) {System.out.println("COULD NOT CLOSE CLIENT GRACEFULLY");}
 
     }
