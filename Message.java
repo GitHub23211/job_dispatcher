c93761bad5d59ed5f574a398f2ba9bba76534d5b
Joshua Santos <joshua.santos1@students.mq.edu.au>
2023-03-29 01:48:24 +1100

    Messages class

---
+++ Message.java
@@ -0,0 +1,19 @@
+import java.io.DataOutputStream;
+
+public class Message {
+    private DataOutputStream output;
+    private Buffer buffer;
+
+    Message(DataOutputStream output, Buffer buffer) {
+        this.output = output;
+        this.buffer = buffer;
+    }
+
+    public void send(String msg) {
+        try {
+            output.write((msg + "\n").getBytes());
+            output.flush();
+            buffer.update(msg);
+        } catch (Exception e) {System.out.println("Error @ messages: " + e);}
+    }    
+}
