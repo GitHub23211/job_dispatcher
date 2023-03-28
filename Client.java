d50450d92d5b31cc352b1d348de019a6e7517b33
Joshua Santos <joshua.santos1@students.mq.edu.au>
2023-03-28 16:21:06 +1100

    functions for sending, receiving messages; calling GETS All;

--- Client.java
+++ Client.java
@@ -26,15 +26,42 @@ public class Client {
 
     public void auth() {
         try {
-            String[] msgs = {"HELO\n", "AUTH user\n"};
+            String[] msgs = {"HELO\n", "AUTH user\n", "REDY\n"};
             for(int i = 0; i < msgs.length; i++) {
-                this.output.write(msgs[i].getBytes());
-                this.output.flush();
-                System.out.println("Client says: " + msgs[i]);	
-                buffer = input.readLine();
-                System.out.println("Server says: " + buffer);
+                this.sendMsgs(msgs[i]);
+                this.printMsgs();
             }
         } catch (Exception e) {System.out.println("Error @ authentication: " + e);}
     }
 
+    public void getServerInfo() {
+        try {
+            this.sendMsgs("GETS All\n");
+            this.printMsgs();
+            if(this.buffer.contains("DATA")) {
+                this.sendMsgs("OK\n");
+                this.buffer = this.input.readLine();
+                while(this.input.ready()) {
+                    this.printMsgs();
+                }
+                this.sendMsgs("OK\n");
+            }
+            this.printMsgs();
+        } catch (Exception e) {System.out.println("Error @ GETS All request: " + e);}
+    }
+
+    private void sendMsgs(String msg) {
+        try {
+            this.output.write(msg.getBytes());
+            this.output.flush();
+            System.out.println("Client says: " + msg);	
+        } catch (Exception e) {System.out.println("Error @ sending message to server: " + e);}
+    }
+
+    private void printMsgs() {
+        try {
+            buffer = this.input.readLine();
+            System.out.println("Server says: " + buffer);
+        } catch (Exception e) {System.out.println("Error @ printing server messages: " + e);}
+    }
 }
