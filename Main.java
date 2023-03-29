78fe1e82164e7382b6b1764fff5ec08f7be3180f
Joshua Santos <joshua.santos1@students.mq.edu.au>
2023-03-29 21:04:00 +1100

    fixed

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         String algo = "";
-        if(args[0].isEmpty()) {
+        if(args.length <= 0) {
             algo = "LRR";
         }
         else {
