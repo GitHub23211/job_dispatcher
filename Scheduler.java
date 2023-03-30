be9fa1ad9208448f10452000b42dee59c48bd75d
Joshua Santos <joshua.santos1@students.mq.edu.au>
2023-03-31 02:35:14 +1100

    scheduler gets jobid itself; delegated finding largest server to parser classes

--- Scheduler.java
+++ Scheduler.java
@@ -1,68 +1,38 @@
-import java.util.ArrayList; 
-import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public abstract class Scheduler {
     Buffer buffer;
     Message msg;
+    Parser parser;
     String largestServerName;
-    String maxLargestServer;
-    ArrayList<ArrayList<String>> servers;
-    ArrayList<Server> serversForJob;
-    private Server server;
+    int maxLargestServer;
 
-    Scheduler(Buffer buffer, Message msg) {
+    Scheduler(Buffer buffer, Message msg, Parser parser) {
         this.buffer = buffer;
         this.msg = msg;
-        this.servers = new ArrayList<ArrayList<String>>();
-        this.serversForJob = new ArrayList<Server>();
-        this.server = new Server();
+        this.parser = parser;
         this.largestServerName = "";
-        this.maxLargestServer = "0";
+        this.maxLargestServer = 0;
     }
 
     public abstract void execute();
 
-    public void getLargestServer() {
-        try {
-            msg.send("GETS All");
-            if(buffer.contains("DATA")) {
-                msg.send("OK");
-                while(buffer.isReady()) {
-                    buffer.update();
-                    servers.add(Parser.getServerInfo(buffer.get()));
-                }
-                findLargestServers();
+    public void setServers() {
+        parser.parse();
+        parser.getLargestServer();
+        largestServerName = parser.name;
+        maxLargestServer = parser.max;
+    }
 
-                chooseServer();
-                for(Server server : serversForJob) {
-                    System.out.println(server.getAll());
-                }
-                msg.send("OK");
-            }
-            msg.send("REDY");
-        } catch (Exception e) {System.out.println("Error @ GETS All request: " + e);}
+    public String getJobId(String msg) {
+        String reg = "([a-zA-Z ])+[0-9]+([ ]+[0-9]+)";
+        Pattern p = Pattern.compile(reg);
+        Matcher m = p.matcher(msg);
+        if(m.find()) {
+            return m.group(2);
+        }
+        return "";
     }
     
-    public void findLargestServers() {
-    	    int maxCore = 0;
-    	    Collections.reverse(servers);
-    	    for(ArrayList<String> s : servers) {
-    		if(Integer.parseInt(s.get(2)) >= maxCore) {
-                maxCore = Integer.parseInt(s.get(2));
-                server.setName(s.get(0));
-                server.setId(s.get(1));
-                server.setCores(s.get(2));
-                serversForJob.add(server);
-		    }
-        }
-    }
-
-    public void chooseServer() {
-        largestServerName = serversForJob.get(serversForJob.size()-1).getName();
-        for(Server server : serversForJob) {
-            if(server.getName().equals(largestServerName)) {
-                maxLargestServer = server.getId();
-            }
-        }
-    }
 }
