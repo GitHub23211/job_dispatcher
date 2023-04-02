34e8cf7f961c0853503022154534e55a767f229e
Joshua Santos <joshua.santos1@students.mq.edu.au>
2023-04-02 18:01:10 +1000

    restructured parser super class

--- src/Parser.java
+++ src/Parser.java
@@ -1,27 +1,21 @@
-import java.util.ArrayList; 
 public abstract class Parser {
 
-    public ArrayList<Server> servers;
-    public String name;
-    public int max;
+    protected String name;
+    protected int max;
 
     Parser() {
-        this.servers = new ArrayList<Server>();
         this.name = "";
         this.max = 0;
     }
 
     public abstract void parse();
-    
-    public void getLargestServer() {
-        int maxCores = 0;
-        for(Server s : servers) {
-            int sCores = s.getCores();
-            if(sCores > maxCores) {
-                maxCores = sCores;
-                max = s.getId();
-                name = s.getName();
-            }
-        }
+
+    public String getServerName() {
+        return name;
     }
+
+    public int getMaxServerCount() {
+        return max;
+    }
+
 }
