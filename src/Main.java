ecbbe8ca3bd8b65046c3badbb1fba8caebf85af5
Joshua Santos <joshua.santos1@students.mq.edu.au>
2023-04-03 20:47:59 +1000

    formatting

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,21 @@
 public class Main {
     public static void main(String[] args) {
         try {
-                String algo = "";
-                String parse = "";
+            String algo = "";
+            String parse = "";
 
-                if(args.length <= 1) {
-                    algo = "LRR";
-                    parse = "XML";
-                }
-                else {
-                    algo = args[0];
-                    parse = args[1];
-                }
+            if(args.length < 2) {
+                algo = "LRR";
+                parse = "XML";
+            }
+            else {
+                algo = args[0];
+                parse = args[1];
+            }
 
-                Client client = new Client("localhost", 50000, algo, parse);
-                client.execute();
+            Client client = new Client("localhost", 50000, algo, parse);
+            client.execute();
                 
-        } catch (Exception e) {System.out.println("Usage: \narg 1 - LRR or ATL \n arg 2 - XML or GETS "); e.printStackTrace();}
+        } catch (Exception e) {System.out.println("Usage: \n arg 1 - LRR or ATL \n arg 2 - XML or GETS "); e.printStackTrace();}
     }
 }
