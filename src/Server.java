353895a483263c185d20d0764c410cf41311982e
Joshua Santos <joshua.santos1@students.mq.edu.au>
2023-05-27 15:14:59 +1000

    new method

--- src/Server.java
+++ src/Server.java
@@ -1,18 +1,25 @@
 public class Server {
     public String name;
     public String id;
+    public String state;
     public int cores;
 
-    public Server(String name, String id, int cores) {
+    public Server(String name, String id, String state, int cores) {
         this.name = name;
         this.id = id;
         this.cores = cores;
+        this.state = state;
     }
 
     public Server() {
         name = "error";
         id = "0";
         cores = 0;
+        state = "";
+    }
+
+    public boolean isValid() {
+        return name.contains("error");
     }
 
     public String getNameAndID() {
